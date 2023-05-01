186fb1a977a20ec6595ea3331db3e6dec9e15627
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2023-05-01 20:26:35 +0700

    update config

--- src/main/java/com/nefentus/api/SecureWebApiApplication.java
+++ src/main/java/com/nefentus/api/SecureWebApiApplication.java
@@ -1,9 +1,11 @@
 package com.nefentus.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AutoConfiguration
 public class SecureWebApiApplication {
 
     public static void main(String[] args) {
