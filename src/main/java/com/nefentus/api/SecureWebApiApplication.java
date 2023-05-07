3710d8722ef3226c1fa0e2effea47ca255ab609e
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2023-05-07 20:04:30 +0700

    adding email service (#25)

--- src/main/java/com/nefentus/api/SecureWebApiApplication.java
+++ src/main/java/com/nefentus/api/SecureWebApiApplication.java
@@ -1,12 +1,29 @@
 package com.nefentus.api;
 
+import com.nefentus.api.Services.EmailService;
+import lombok.extern.slf4j.Slf4j;
+import microsoft.exchange.webservices.data.autodiscover.IAutodiscoverRedirectionUrl;
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
+import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
+import microsoft.exchange.webservices.data.credential.WebCredentials;
+import microsoft.exchange.webservices.data.property.complex.MessageBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URI;
+
 @SpringBootApplication
 @AutoConfiguration
+@Slf4j
 public class SecureWebApiApplication {
+
+    @Autowired
+    private EmailService emailService;
 
     public static void main(String[] args) {
         SpringApplication.run(SecureWebApiApplication.class, args);
