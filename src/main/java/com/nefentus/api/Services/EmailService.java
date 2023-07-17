eea6a7ae257bbd0a5919218a6a521e55e86ad4ca
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2023-07-17 15:15:07 +0200

    Replace MicrososftExchange email service by SMTP based email service

--- src/main/java/com/nefentus/api/Services/EmailService.java
+++ src/main/java/com/nefentus/api/Services/EmailService.java
@@ -1,61 +1,48 @@
 package com.nefentus.api.Services;
 
 import lombok.extern.slf4j.Slf4j;
-import microsoft.exchange.webservices.data.autodiscover.IAutodiscoverRedirectionUrl;
-import microsoft.exchange.webservices.data.core.ExchangeService;
-import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
-import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
-import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
-import microsoft.exchange.webservices.data.credential.WebCredentials;
-import microsoft.exchange.webservices.data.property.complex.MessageBody;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import javax.annotation.PostConstruct;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.InternetAddress;
 
 @Service
 @Slf4j
 public class EmailService {
+	@Value("${spring.mail.username}")
+	private String username;
 
-    private ExchangeService service;
-    @Value("${spring.mail.username}")
-    private String defaultEmail;
-    @Value("${spring.mail.password}")
-    private String defaultEmailPass;
+	@Autowired
+	private JavaMailSender mailSender;
 
-    @PostConstruct
-    public void initEmailService() {
-        try {
-            log.info("Init connect to host mail...");
-            service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
-            ExchangeCredentials credentials = new WebCredentials(defaultEmail, defaultEmailPass);
-            service.setCredentials(credentials);
-            service.autodiscoverUrl(defaultEmail, new RedirectionUrlCallback());
-        } catch (Exception e) {
-            log.error("Error init connect to email host", e);
-        }
-    }
+	@PostConstruct
+	public void initEmailService() {
+		try {
+			MimeMessage message = mailSender.createMimeMessage();
+		} catch (Exception e) {
+			log.error("Error init connect to email host", e);
+		}
+	}
 
-    static class RedirectionUrlCallback implements IAutodiscoverRedirectionUrl {
-        public boolean autodiscoverRedirectionUrlValidationCallback(
-                String redirectionUrl) {
-            return redirectionUrl.toLowerCase().startsWith("https://");
-        }
-    }
+	@Async
+	public void sendEmail(String toEmail, String subject, String body) {
+		MimeMessage message = mailSender.createMimeMessage();
 
-    @Async
-    public void sendEmail(String toEmail, String subject, String body) {
-        log.info("Start send email to {}", toEmail);
-        EmailMessage msg;
-        try {
-            msg = new EmailMessage(service);
-            msg.setSubject(subject);
-            msg.setBody(MessageBody.getMessageBodyFromText(body));
-            msg.getToRecipients().add(toEmail);
-            msg.send();
-        } catch (Exception e) {
-            log.error("Email error", e);
-        }
-    }
+		try {
+			message.setFrom(new InternetAddress(username, "Nefentus"));
+			message.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(toEmail));
+			message.setSubject(subject);
+			message.setContent(body, "text/html; charset=utf-8");
+			mailSender.send(message);
+		} catch (Exception e) {
+			log.error("Email error", e);
+			return;
+		}
+
+		log.info("Email sent successfully to {}", toEmail);
+	}
 }
